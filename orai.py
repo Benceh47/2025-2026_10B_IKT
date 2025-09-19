f91aae6c809ceef2ed6d27bf12c54db15da9e529
Ribárik Bence <ribarik.bence@ckik.loc>
2025-09-19 11:23:21 +0200

    legyszi

--- orai.py
+++ orai.py
@@ -1,23 +1,18 @@
 print("órai munka")
-<<<<<<< HEAD
 nev = "Nagy Rozália"
-=======
 nev = "Kis Pista"g  
->>>>>>> orai
 b = true
 a = 13
 print("sziasztok, ez ötös")
 print("2025.09.19")
 print("kérek egy ötöst")
 b=13
-<<<<<<< HEAD
 c=14
 d=12
-=======
 c="szöveg"
 d=15
->>>>>>> 8b4ef2f24ad633f3501ca491be10e17b3a8ac8f2
 e=16
 f=17
 g =18
 h=19 
+
